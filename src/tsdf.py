edf2c5a62ffbc7f4dfac2d522305afa39f97e1ea
shafthors <shafthors@student.ethz.ch>
2021-03-31 11:55:16 +0200

    add read_write_model.py from colmap, read from images.txt, compute translation matrices

--- src/tsdf.py
+++ src/tsdf.py
@@ -1,6 +1,7 @@
 
 import open3d as o3d
 import numpy as np
+from read_write_model import read_images_text
 
 #%%
 
@@ -47,11 +48,88 @@ pcd = o3d.io.read_point_cloud("../../3D-data/sfm_fused.ply")
 #print(np.asarray(pcd.points))
 visualize_cloud(pcd)
 
+#%%
+
+# This function is based on this tutorial:
+# https://automaticaddison.com/how-to-convert-a-quaternion-to-a-rotation-matrix/
+def quaternion_rotation_matrix(Q):
+    """
+    Covert a quaternion into a full three-dimensional rotation matrix.
+
+    Input
+    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
+
+    Output
+    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
+             This rotation matrix converts a point in the local reference 
+             frame to a point in the global reference frame.
+    """
+    # Extract the values from Q
+    q0 = Q[0]
+    q1 = Q[1]
+    q2 = Q[2]
+    q3 = Q[3]
+	
+    # First row of the rotation matrix
+    r00 = 2 * (q0 * q0 + q1 * q1) - 1
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+	
+    # Second row of the rotation matrix
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = 2 * (q0 * q0 + q2 * q2) - 1
+    r12 = 2 * (q2 * q3 - q0 * q1)
+	
+    # Third row of the rotation matrix
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = 2 * (q0 * q0 + q3 * q3) - 1
+	
+    # 3x3 rotation matrix
+    rot_matrix = np.array([[r00, r01, r02],
+                           [r10, r11, r12],
+                           [r20, r21, r22]])
+						   
+    return rot_matrix
+
+#%%
+
+images = read_images_text("../../3D-data/images.txt")
+
+#%%
+
+quaternion = []
+translation = []
+for i in range(len(images)):
+    quaternion.append(images[i+1][1])
+    translation.append(images[i+1][2])
+
+quaternion = np.array(quaternion)
+translation = np.array(translation)
+
+print(quaternion.shape)
+print(translation.shape)
+
+#%%
+
+transformation_matrix = []
+for i in range(len(quaternion)):
+    rotation_matrix = quaternion_rotation_matrix(quaternion[i])
+    transformation = np.c_[rotation_matrix, translation[i]]
+    transformation = np.vstack((transformation, np.array([0,0,0,1])))
+    transformation_matrix.append(transformation)
+
+#%% Try to visualize depthmaps from COLMAP, using their scripts/python/read_dense.py
+
+#from read_write_dense import read_array
+
+#read_array('../../3D-data/depth_maps/')
+
 #%% Follow this tutorial
 #http://www.open3d.org/docs/release/tutorial/pipelines/rgbd_integration.html#TSDF-volume-integration
 
 # TODO: Find out if we have this or how this is generated
-camera_poses = read_trajectory("odometry.log") # Use example from tutorial
+camera_poses = read_trajectory("../../3D-data/odometry.log") # Use example from tutorial
 
 #%% TSDF volume integration
 #TODO: Find out how to access the depthmaps
@@ -87,10 +165,12 @@ o3d.visualization.draw_geometries([mesh],
 
 
 
+#%%
+def main():
+    print()
 
-
-
-
+if __name__ == '__main__':
+    main()
 
 
 
