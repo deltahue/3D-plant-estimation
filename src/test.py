3117fe53dae31dcdff7ae168ffa236118463a557
shafthors <shafthors@student.ethz.ch>
2021-04-01 14:08:28 +0200

    fix trajectories, put functions into separate file, perform tests, tsdf still not working

---
+++ src/test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  1 13:38:55 2021
+
+@author: soley
+"""
+import numpy as np
+
+test_transformation_matrices = []
+'''
+# Case 1, only translations, get transformation 3,3,3 as expecte
+test_transformation_matrices.append(np.array([[1,0,0,1],[0,1,0,2],[0,0,1,3],[0,0,0,1]]))
+test_transformation_matrices.append(np.array([[1,0,0,4],[0,1,0,5],[0,0,1,6],[0,0,0,1]]))
+
+# Case 2, one translation, one rotation 90 degrees about x-axis, must translate back
+test_transformation_matrices.append(np.array([[1,0,0,1],[0,1,0,2],[0,0,1,3],[0,0,0,1]]))
+test_transformation_matrices.append(np.array([[1,0,0,0],[0,0,-1,0],[0,1,0,0],[0,0,0,1]]))
+'''
+# Case 3: rotate first and then translate (must nown roatate back)
+test_transformation_matrices.append(np.array([[1,0,0,0],[0,0,-1,0],[0,1,0,0],[0,0,0,1]]))
+test_transformation_matrices.append(np.array([[1,0,0,1],[0,1,0,2],[0,0,1,3],[0,0,0,1]]))
+
+#test_transformation_matrices.append(np.array([[],[],[],[]]))
+test_transformation_matrices = np.array(test_transformation_matrices)
+
+# Convert the world coordinate matrices into trajectories
+test_trajectories = []
+test_trajectories.append(np.eye(4)) # First frame is the reference
+for i in range((len(test_transformation_matrices)-1)):
+    trans = np.matmul(test_transformation_matrices[i+1],np.linalg.inv(test_transformation_matrices[i]))
+    test_trajectories.append(trans)
+
+test_trajectories = np.array(test_trajectories)
+print(test_trajectories)
--- src/tsdf.py
+++ src/tsdf.py
@@ -2,76 +2,29 @@
 # import packages
 import open3d as o3d
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2 as cv
 
 # colmap
 from read_write_model import read_images_text, read_cameras_text
 from read_write_dense import read_array
 
-#%%
+# Useful functions
+from utils import visualize_cloud, quaternion_rotation_matrix
 
-# Function to visualize point cloud (also works for mesh)
-def visualize_cloud(pcd):
-    o3d.visualization.draw_geometries([pcd],
-                                  zoom=0.1500,
-                                  front= [-0.4257, 0.2125, -0.7000],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])    
-    
-#%%
+visualize_pcd = False
+visualize_mesh = False
+save_depth_maps = False
 
-# This function is based on this tutorial:
-# https://automaticaddison.com/how-to-convert-a-quaternion-to-a-rotation-matrix/
-def quaternion_rotation_matrix(Q):
-    """
-    Covert a quaternion into a full three-dimensional rotation matrix.
+# Visualize the point cloud
+if visualize_pcd == True:
+    print("Load a ply point cloud, print it, and render it")
+    pcd = o3d.io.read_point_cloud("../../3D-data/sfm_fused.ply")
+    visualize_cloud(pcd)
 
-    Input
-    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
-
-    Output
-    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
-             This rotation matrix converts a point in the local reference 
-             frame to a point in the global reference frame.
-    """
-    # Extract the values from Q
-    q0 = Q[0]
-    q1 = Q[1]
-    q2 = Q[2]
-    q3 = Q[3]
-	
-    # First row of the rotation matrix
-    r00 = 2 * (q0 * q0 + q1 * q1) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
-	
-    # Second row of the rotation matrix
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0 * q0 + q2 * q2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
-	
-    # Third row of the rotation matrix
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0 * q0 + q3 * q3) - 1
-	
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02],
-                           [r10, r11, r12],
-                           [r20, r21, r22]])
-						   
-    return rot_matrix
-
-#%% Visualize the point cloud
-
-print("Load a ply point cloud, print it, and render it")
-pcd = o3d.io.read_point_cloud("../../3D-data/sfm_fused.ply")
-visualize_cloud(pcd)
-
-#%% Read information about camera poses, convert information into translation matrices
-
+# Read information about camera poses
 images = read_images_text("../../3D-data/images.txt")
 
+# Convert information into (world coordinate) transformation matrices
 quaternion = []
 translation = []
 for i in range(len(images)):
@@ -90,7 +43,17 @@ for i in range(len(quaternion)):
 
 transformation_matrices = np.array(transformation_matrices)
 
-#%%
+# Convert the world coordinate matrices into trajectories
+trajectories = []
+trajectories.append(np.eye(4)) # First frame is the reference
+for i in range((len(transformation_matrices)-1)):
+    trans = np.matmul(transformation_matrices[i+1],np.linalg.inv(transformation_matrices[i]))
+    trajectories.append(trans)
+
+trajectories = np.array(trajectories)
+
+
+# Read information about camera intrinsics
 cameras = read_cameras_text("../../3D-data/cameras.txt")
 
 widths, heights, focals, cxs,cys = [],[],[],[],[]
@@ -107,56 +70,47 @@ focals = np.array(focals)
 cxs = np.array(cxs)
 cys = np.array(cys)
 
-#%% Visualize (and save) depthmaps from COLMAP, using their scripts/python/read_dense.py
-
+# Get (and save) depth maps from COLMAP, using their scripts/python/read_dense.py
 depth_maps = []
-save = False
 for i in range(len(transformation_matrices)):
-    depth_map = read_array('../../3D-data/depth_maps/frame-'+ f'{i+1:05}' + '.jpg.geometric.bin')
+    depth_map = read_array('../../3D-data/depth_maps/frame-'+ f'{i+1:05}' + '.jpg.photometric.bin')
     depth_maps.append(depth_map)
-    if save == True:
-        plt.imshow(depth_map)
-        plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-        plt.savefig('../../3D-data/depth_images/frame-'+ f'{i+1:05}' + '.png')
+    if save_depth_maps == True:
+        cv.imwrite('../../3D-data/depth_images/frame-'+ f'{i+1:05}' + '.jpg', depth_map)
         
 depth_maps = np.array(depth_maps)
 
-#%% Follow this tutorial for TSDF volume integration:
+# Follow this tutorial for TSDF volume integration:
 # http://www.open3d.org/docs/release/tutorial/pipelines/rgbd_integration.html#TSDF-volume-integration
 
 volume = o3d.pipelines.integration.ScalableTSDFVolume(
-    voxel_length=4.0 / 512.0,
-    sdf_trunc=0.04,
+    voxel_length = 0.01, # 4.0 / 512.0, 
+    sdf_trunc = 0.05, #0.04, 
     color_type=o3d.pipelines.integration.TSDFVolumeColorType.RGB8)
 
 for i in range(len(transformation_matrices)):
-    print("Integrate {:d}-th image into the volume.".format(i))
+    if i % 10 == 0 or i == len(transformation_matrices) -1:
+        print("Integrate image no {:d} into the volume.".format(i))
     color = o3d.io.read_image('../../3D-data/images/frame-' + f'{i+1:05}' + '.jpg')
-    depth = o3d.geometry.Image((depth_maps[i]).astype(np.uint8))
+    depth = o3d.io.read_image('../../3D-data/depth_images/frame-' + f'{i+1:05}' + '.png')
     rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
         color, depth, depth_trunc=4.0, convert_rgb_to_intensity=False)
-
     intrinsics = o3d.cpu.pybind.camera.PinholeCameraIntrinsic()
-    #intrinsics.set_intrinsics(widths[i], heights[i],focals[i],focals[i],cxs[i],cys[i])
     intrinsics.set_intrinsics(len(depth_maps[i][0]), len(depth_maps[i]),focals[i],focals[i],cxs[i],cys[i])
-    #intrinsics = o3d.camera.PinholeCameraIntrinsic().set_intrinsics(widths[i], heights[i],focals[i],focals[i],cxs[i],cys[i])
     volume.integrate(
         rgbd,
         intrinsics,
-        np.linalg.inv(transformation_matrices[i]))
+        np.linalg.inv(trajectories[i])) # was transformation_matrices
 
-
-#%% Extract a mesh and visualize it
+# Extract a mesh and visualize it
 print("Extract a triangle mesh from the volume and visualize it.")
 mesh = volume.extract_triangle_mesh()
 mesh.compute_vertex_normals()
-#visualize_cloud(mesh)
 
-o3d.visualization.draw_geometries([mesh],
-                                  front=[0.5297, -0.1873, -0.8272],
-                                  lookat=[2.0712, 2.0312, 1.7251],
-                                  up=[-0.0558, -0.9809, 0.1864],
-                                  zoom=0.47)
+print(mesh)
+
+if visualize_mesh == True:
+    visualize_cloud(mesh)
 
 
 
