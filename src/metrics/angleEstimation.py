a30f099972068fc1befca91a1be3a0c124fffbd7
helmd <david.e.helm@outlook.com>
2021-06-14 19:42:37 +0200

    cleanup

--- src/metrics/angleEstimation.py
+++ src/metrics/angleEstimation.py
@@ -9,18 +9,15 @@ def findAngle(leafPath, normal):
     P = np.asarray(mesh.vertices)
     tris = np.asarray(mesh.triangles)
     arr = []
-    i = 0
     mesh.compute_triangle_normals(normalized=True)
     mesh2.orient_normals_to_align_with_direction()
     
     tri_normals = np.asarray(mesh.triangle_normals)
     normal = -1 * normal
     for tri in tri_normals:
-
         #A.B = |A|x|B|x cos(X)
-        vecNor = tri_normals[i]
+        vecNor = tri
         cosAng = np.dot(vecNor, normal)/math.sqrt((np.dot(vecNor, vecNor) * np.dot(normal, normal)))
-        i += 1
         ang = math.degrees(math.acos(cosAng))
         if(ang > 90):
             ang = 180 - ang
